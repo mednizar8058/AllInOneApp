70e6b782491f27ca737d1fab1e491e38d697cd7a
MedNizar <rockstarhard4@gmail.com>
2021-06-20 12:38:49 +0100

    Initial Commit

---
+++ AllInOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllInOne
+//
+//  Created by MNizar on 6/20/21.
+//  Copyright © 2021 MNizar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
